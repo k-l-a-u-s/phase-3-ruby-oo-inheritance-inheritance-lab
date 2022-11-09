990c592dc8d2d63ddefe9f41bf0dee1d6c17fd5a
Nicholas Ndikwe <nicholasndikwe@gmail.com>
2022-11-10 01:39:54 +0300

    completed assignment

---
+++ lib/user.rb
@@ -0,0 +1,9 @@
+class User
+    attr_accessor :first_name, :last_name
+    
+         def name(first_name, last_name)
+             @first_name = name
+             @last_name = name
+         end
+    
+    end
